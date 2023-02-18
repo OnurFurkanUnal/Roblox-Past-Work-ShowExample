fb2c67f64fb9e9d693d9cd5b38e1e2c36ffae14b
OnurFurkanUnal <onurfurkanunal@gmail.com>
2023-02-18 03:18:31 +0300

    Add files via upload

---
+++ pastwork.lua
@@ -0,0 +1,316 @@
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local testEvent = replicatedStorage:WaitForChild("TestEvent")
+local plyr = game:GetService("Players")
+local dataStoreServices = game:GetService("DataStoreService")
+local dataStore = dataStoreServices:GetOrderedDataStore("st")
+local dataStore1 = dataStoreServices:GetOrderedDataStore("st1")
+local dataStorePlyr = dataStoreServices:GetDataStore("Player")
+local loaded = {}
+local ServerStorage = game:GetService("ServerStorage")
+local InventoryTemplate = ServerStorage:WaitForChild("InventoryTemplate")
+local Items = replicatedStorage:WaitForChild("Items")
+local TweenService = game:GetService("TweenService")
+local PickupItem = replicatedStorage:WaitForChild("PickupItem")
+local DropItem = replicatedStorage:WaitForChild("RemoteFunctionDropItem")
+local pickupTweenInfo = TweenInfo.new(
+	0.3,
+	Enum.EasingStyle.Linear,
+	Enum.EasingDirection.In,
+	0,
+	false,
+	0
+)
+local UIS = game:GetService("UserInputService")
+local pickupKey = "F"
+local player = game.Players:GetPlayerFromCharacter(script.Parent)
+local mouse = player:GetMouse()
+local PlayerGui = player:WaitForChild("PlayerGui")
+local PickUpInfoGui = PlayerGui:WaitForChild("PickUpInfoGui")
+local Inventory = player.Inventory
+local MainGui = script.Parent
+local InventoryGui = MainGui.InventoryGui
+local InventoryAcKapaButton = MainGui.TextButton
+local part = game.Workspace:WaitForChild("Part")
+
+wait(3)
+local success, errorMessage = pcall(function()
+	local data = dataStore:GetSortedAsync(false,5)
+	local winsPage = data:GetCurrentPage()
+
+	for rank, data in ipairs(winsPage) do
+		local name = data.key
+		local Gold = data.value
+		local successs, val = pcall(plyr.GetNameFromUserIdAsync, plyr, data.key)
+		if successs == true then
+			local b = dataStore1:GetAsync(data.key)
+			-- Show players on leaderboard object
+			local newLbFrame = game.ReplicatedStorage:WaitForChild("LeaderBoardFrame"):Clone()
+			newLbFrame.Player.Text = val
+			newLbFrame.Rank.Text = rank
+			newLbFrame.Gold.Text = Gold
+			newLbFrame.Frkn.Text = b
+			newLbFrame.Position = UDim2.new(0,0,newLbFrame.Position.Y.Scale + (0.8 * #game.Workspace.GlobalLeaderBoard.LeaderBoardGUI.Holder:GetChildren()),0)
+			newLbFrame.Parent = game.Workspace.GlobalLeaderBoard.LeaderBoardGUI.Holder
+		end
+	end
+end)
+
+local function SetupInventory(player)
+	local Inventory = player:WaitForChild("Inventory")
+	local PlayerGui = player:WaitForChild("PlayerGui")
+	local MainGui = PlayerGui:WaitForChild("MainGui")
+	local InventoryGui = MainGui:WaitForChild("InventoryGui")
+	
+	for i,item in pairs(Inventory:GetChildren()) do
+		local itemGui = InventoryGui.Templates.Item:Clone()
+		itemGui.Name = item.Name
+		itemGui.ItemName.Text = item.Name
+		itemGui.ItemQuantity.Text = item.Value
+		
+		if item.Value > 0 then
+			itemGui.Visible = true
+			itemGui.Parent = InventoryGui.ItemList
+		else
+			itemGui.Visible = false
+			itemGui.Parent = InventoryGui.ItemList
+		end
+	end
+end
+
+game.Players.PlayerAdded:Connect(function(player)
+    --create data on player
+	local success, value = pcall(dataStorePlyr.GetAsync,dataStorePlyr,player.UserId)
+	if success == false then player:Kick("DataStore failed to load") return end
+	local data = value or {}
+	local st = Instance.new("Folder")
+	st.Name = "st"
+	st.Parent = player
+	
+	local gold = Instance.new("IntValue")
+	gold.Name = "Gold"
+	gold.Value = 0
+	gold.Parent = st
+	
+	local Frkn = Instance.new("IntValue")
+	Frkn.Name = "Frkn"
+	Frkn.Value = 0
+	Frkn.Parent = st
+	print("Loaded:", value)
+	for i, folder in game.ServerStorage.PlayerData:GetChildren() do
+		local subData = data[folder.Name] or {} -- subdata datanın ıcınde dosya adıyla yoksa boş table olarak ata dedik burada
+		local clone = folder:Clone()
+		for i , child in clone:GetChildren()do
+			child.Value = subData[child.Name] or child.Value
+			if clone.Name == "leaderstats" then
+				if child.Name == "Gold" then
+					gold.Value = child.Value
+				elseif child.Name == "Frkn" then
+					Frkn.Value = child.Value
+				end
+			end
+		end
+		clone.Parent = player
+	end
+	local success, value = pcall(dataStore.SetAsync, dataStore, player.UserId,gold.Value)
+	local success, value = pcall(dataStore1.SetAsync, dataStore1, player.UserId,Frkn.Value)
+	loaded[player] = true
+	
+	--add point to user data when player join the game
+	local data2 = player:WaitForChild("Data2",10)
+	if data2 == nil then return end
+	data2.LoginCounter.Value += 1
+	
+	local data3 = player:WaitForChild("leaderstats",10)
+	if data3 == nil then return end 
+	data3.Gold.Value += 1
+	
+	local data4 = player:WaitForChild("leaderstats",10)
+	if data4 == nil then return end
+	data4.Frkn.Value += 1
+	
+	--Create inventorytemplate on player
+	local GameData = InventoryTemplate:Clone()
+	GameData.Name = "Inventory"
+	GameData.Parent = player	
+	SetupInventory(player)
+end)
+
+--Save data when player left game
+game.Players.PlayerRemoving:Connect(function(player)
+	if loaded[player] == nil then return end
+	local data = {}
+	for i, folder in game.ServerStorage.PlayerData:GetChildren() do
+		local subData = {}
+		for i, child in player[folder.Name]:GetChildren() do
+			subData[child.Name] = child.Value
+			if child.Name == "Gold" then
+				local success, value = pcall(dataStore.SetAsync, dataStore, child.Value)
+			elseif child.Name == "Frkn" then
+				local success, value = pcall(dataStore1.SetAsync, dataStore1, child.Value)
+			end
+		end
+		data[folder.Name] = subData
+		player[folder.Name]:Destroy()
+	end
+	local success, value = pcall(dataStorePlyr.SetAsync, dataStorePlyr, player.UserId,data)
+	print("Saved:",data)
+	loaded[player] = nil
+end)
+
+--Check itemValues changed if 0 set visible false else visible true
+for i , itemValue in pairs(Inventory:GetChildren()) do
+	itemValue.Changed:Connect(function()
+		local itemGui = InventoryGui.ItemList:FindFirstChild(itemValue.Name)
+		if itemGui then
+			itemGui.ItemQuantity.Text = itemValue.Value
+			if itemValue.Value <= 0 then
+				itemGui.Visible = false
+			else
+				itemGui.Visible = true
+			end
+		end
+	end)
+end
+
+-- assign events to inventory items buttons to drop them
+for i , itemButton in pairs(InventoryGui.ItemList:GetDescendants()) do	
+	if itemButton:IsA("ImageButton") then
+		itemButton.MouseButton2Up:Connect(function()
+			local itemFrame = itemButton.Parent
+			local itemValue = Inventory:FindFirstChild(itemFrame.Name)
+			if itemValue.Value > 0 then
+				local DropItemm = DropItem:InvokeServer(itemFrame.Name)
+				if DropItemm == true then
+					if itemValue.Value > 0 then
+						itemFrame.ItemQuantity.Text = itemValue.Value
+					else
+						itemFrame.Visible = false
+					end
+				end
+			end
+		end)
+		itemButton.MouseButton1Up:Connect(function()
+			local itemFrame = itemButton.Parent
+			local itemValue = Inventory:FindFirstChild(itemFrame.Name)
+			if itemValue.Value > 0 then
+				local DropItemm = DropItem:InvokeServer(itemFrame.Name)
+				if DropItemm == true then
+					if itemValue.Value > 0 then
+						itemFrame.ItemQuantity.Text = itemValue.Value
+					else
+						itemFrame.Visible = false
+					end
+				end
+			end
+		end)
+	end
+end
+
+-- when pickupitem fired here will work
+PickupItem.OnServerEvent:Connect(function(player, item)
+	local char = player.Character
+	local inventory = player.Inventory
+	local itemValue = inventory:FindFirstChild(item.Name)
+	if itemValue then
+		itemValue.Value = itemValue.Value + 1
+		
+		local finishingLocation = char.HumanoidRootPart.CFrame
+		item.CanCollide = false
+		item.Anchored = true
+		local tween = TweenService:Create(item, pickupTweenInfo, {Transparency = 1,Size = Vector3.new(0,0,0), CFrame = finishingLocation})
+		tween:Play()
+	end
+end)
+
+-- when dropitem fired here will work
+DropItem.OnServerInvoke = function(player,itemName)
+	local Inventory = player.Inventory
+	local item = Inventory:FindFirstChild(itemName)
+	if item then
+		if item.Value > 0 then
+			item.Value = item.Value - 1
+			local itemClone = Items:FindFirstChild(itemName):Clone()
+			itemClone.CFrame = player.Character.HumanoidRootPart.CFrame + player.Character.HumanoidRootPart.CFrame.LookVector * 6
+			itemClone.Parent = game.Workspace
+			return true
+		else
+			return false
+		end
+	end
+end
+
+--show player to item name and that "f to pickup" text under true conditions
+UIS.InputChanged:Connect(function(input)
+	if mouse.Target then
+		if mouse.Target:FindFirstChild("Pickable") then
+			local item = mouse.Target
+			PickUpInfoGui.Adornee = item
+			PickUpInfoGui.ObjectName.Text = item.Name
+			PlayerGui.PickUpInfoGui.Enabled = true
+		else
+			PickUpInfoGui.Adornee = nil
+			PlayerGui.PickUpInfoGui.Enabled = false
+		end
+	end
+	
+end)
+
+--fire pickupitem event under true conditions
+UIS.InputEnded:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode[pickupKey] then
+		if mouse.Target then
+			if mouse.Target:FindFirstChild("Pickable") then
+				local item = mouse.Target
+				if item then
+					local distanceFromItem = player:DistanceFromCharacter(item.Position)
+					if distanceFromItem < 30 then
+						--take item
+						PickupItem:FireServer(item)
+					end
+				end
+			end
+		end
+	end
+end)
+
+UIS.InputBegan:Connect(function(input)
+	-- if user pressed delete key spesific part 
+	if input.KeyCode == Enum.KeyCode.Delete then
+		testEvent:FireServer(part)
+	end
+	
+end)
+
+--Appear and Dissappear inventory
+OpenCloseInventory.MouseButton2Up:Connect(function()
+	if InventoryGui.Visible == true then
+		InventoryGui.Visible = false
+	else
+		InventoryGui.Visible = true
+	end
+	
+end)
+OpenCloseInventory.MouseButton1Up:Connect(function()
+	if InventoryGui.Visible == true then
+		InventoryGui.Visible = false
+	else
+		InventoryGui.Visible = true
+	end
+
+end)
+
+testEvent.OnServerEvent:Connect(function(player,part)
+    --delete object from server(from all users)
+	part:Destroy()
+end)
+
+game:BindToClose(function()
+	while next(loaded) ~= nil do
+		task.wait()
+	end
+end)
+
+
+
+
+
